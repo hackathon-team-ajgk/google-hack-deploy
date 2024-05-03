fff5f1e7ac21780a94b2a61a04b63daa8f4f3427
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-05-03 00:51:28 -0600

    cahnged cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,10 @@ app.use(express.json());
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://google-hack-deploy-frontend.vercel.app/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
